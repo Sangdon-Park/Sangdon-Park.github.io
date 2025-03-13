10f7c93f5b070d1bdcf3ec9558ebaaebe0c3ee01
sayberrygames <sayberrygames@gmail.com>
2025-03-13 16:03:53 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -224,42 +224,154 @@ function loadPublications() {
     
     if (!publicationsContainer) return;
     
-    // Sample parsed publications data structure from publications.md
+    // Parsed publications data structure from publications.md
     const publicationsData = [
         // Journals
         {
             title: "Differential Pricing-based Task Offloading for Delay-Sensitive IoT Applications in Mobile Edge Computing System",
             authors: "Hyeonseok Seo, Hyeontaek Oh, Jun Kyun Choi, <em>Sangdon Park*</em>",
-            journal: "IEEE Internet of Things Journal, 2022",
+            journal: "IEEE Internet of Things Journal, 2022 (Impact Factor 9.471, 3.416% for 2020 JCR Ranking)",
             link: "#",
             type: "journal"
         },
         {
             title: "Energy scheduling scheme for a charging facility considering the satisfaction of electric vehicle users",
             authors: "Jangkyum Kim, Joohyung Lee*, <em>Sangdon Park</em>, Jun Kyun Choi",
-            journal: "IEEE ACCESS, 2021",
+            journal: "IEEE ACCESS, 2021 (Impact Factor 3.367, 34.249% for 2020 JCR Ranking)",
             link: "#",
             type: "journal"
         },
         {
             title: "Lane Detection Aided Online Dead Reckoning for GNSS Denied Environments",
             authors: "Jinhwan Jeon, Yoonjin Hwang, Yongseop Jeong, <em>Sangdon Park</em>, In So Kweon, Seibum Choi*",
-            journal: "Sensors, vol. 21, no. 20, pp. 6805, October, 2021",
+            journal: "Sensors, vol. 21, no. 20, pp. 6805, October, 2021 (Impact Factor 3.576, 21.094% for 2020 JCR Ranking)",
             link: "#",
             type: "journal"
         },
         {
             title: "A Multivariate Time Series Prediction-Based Adaptive Data Transmission Period Control Algorithm for IoT Networks",
-            authors: "Jaeseob Han, Gyeong Ho Lee, <em>Sangdon Park* (co-corresponding)</em>, Joohyung Lee*, Jun Kyun Choi",
-            journal: "IEEE Internet of Things Journal, 2021",
+            authors: "Jaeseob Han, Gyeong Ho Lee, <em>Sangdon Park*(co-corresponding)</em>, Joohyung Lee*, Jun Kyun Choi",
+            journal: "IEEE Internet of Things Journal, Early Access (Impact Factor 9.471, 3.416% for 2020 JCR Ranking)",
             link: "https://ieeexplore.ieee.org/document/9598874",
             type: "journal"
         },
         {
             title: "Joint Subcarrier and Transmission Power Allocation in OFDMA-based WPT System for Mobile Edge Computing in IoT Environment",
             authors: "Jaeseob Han, Gyeong Ho Lee, <em>Sangdon Park*</em>, Jun Kyun Choi",
-            journal: "IEEE Internet of Things Journal, 2021",
+            journal: "IEEE Internet of Things Journal, Early Access (Impact Factor 9.471, 1.613% for 2020 JCR Ranking)",
             link: "https://ieeexplore.ieee.org/abstract/document/9516698",
+            type: "journal"
+        },
+        {
+            title: "Three Dynamic Pricing Schemes for Resource Allocation of Edge Computing for IoT Environment",
+            authors: "Beomhan Baek, Joohyung Lee, Yuyang Peng, <em>Sangdon Park*</em>",
+            journal: "IEEE Internet of Things Journal, vol. 7, no. 05., pp. 4292-4303, May, 2020 (Impact Factor 9.471, 3.416% for 2020 JCR Ranking)",
+            link: "https://ieeexplore.ieee.org/document/8959172",
+            type: "journal"
+        },
+        {
+            title: "Competitive Data Trading Model with Privacy Valuation for Multiple Stakeholders in IoT Data Markets",
+            authors: "Hyeontaek Oh, <em>Sangdon Park*</em>, Gyu Myoung Lee, Jun Kyun Choi, and Sungkee Noh",
+            journal: "IEEE Internet of Things Journal, vol. 7, no. 04., pp. 3623-3639, April, 2020 (Impact Factor 9.471, 3.416% for 2020 JCR Ranking)",
+            link: "https://ieeexplore.ieee.org/document/8998246/",
+            type: "journal"
+        },
+        {
+            title: "Battery Wear Model based Energy Trading in Electric Vehicles: A Naive Auction Model and a Market Analysis",
+            authors: "Jangkyum Kim, Joohyung Lee*, <em>Sangdon Park</em>, and Jun Kyun Choi",
+            journal: "IEEE Transactions on industrial informatics, vol. 15, no. 07, pp. 4140 - 4151, July, 2019 (Impact Factor 5.43, 2.13% for 2017 JCR Ranking)",
+            link: "https://ieeexplore.ieee.org/document/8546796",
+            type: "journal"
+        },
+        {
+            title: "Optimal Throughput Analysis of Multiple Channel Access in Cognitive Radio Networks",
+            authors: "<em>Sangdon Park</em>, Ganguk Hwang*, and Jun Kyun Choi",
+            journal: "Annals of Operations Research, vol. 277, no. 02, pp. 345-370, June, 2019 (Impact Factor 1.864, 38.55% for 2017 JCR Ranking)",
+            link: "https://doi.org/10.1007/s10479-017-2648-3",
+            type: "journal"
+        },
+        {
+            title: "Energy-Efficient Cooperative Transmission for Intelligent Transportation Systems",
+            authors: "Yuyang Peng, Jun Li*, <em>Sangdon Park</em>, Konglin Zhu, Mohammad Mehedi Hassan, and Ahmed Alsanad",
+            journal: "Future Generation Computer Systems, vol. 94, pp. 634-640, May, 2019 (Impact Factor 4.639, 6.80% for 2017 JCR Ranking)",
+            link: "https://www.sciencedirect.com/science/article/pii/S0167739X18321319",
+            type: "journal"
+        },
+        {
+            title: "Comparison Between Seller and Buyer Pricing Systems for Energy Trading in Microgrids",
+            authors: "Sohee Bae, and <em>Sangdon Park*</em>",
+            journal: "IEEE Access, vol. 07, pp. 54084-54096, April, 2019 (Impact Factor 3.557, 16.22% for 2017 JCR Ranking)",
+            link: "https://ieeexplore.ieee.org/document/8695014",
+            type: "journal"
+        },
+        {
+            title: "Power Efficient Clustering Scheme for 5G Mobile Edge Computing Environment",
+            authors: "Jaewon Ahn, Joohyung Lee*, <em>Sangdon Park</em>, and Hong-sik Park",
+            journal: "Springer Mobile Networks & Applications, vol. 24, no. 02, pp 643–652, April, 2019 (Impact Factor 2.497, 25.00% for 2017 JCR Ranking)",
+            link: "https://link.springer.com/article/10.1007/s11036-018-1164-2",
+            type: "journal"
+        },
+        {
+            title: "Personal Data Trading Scheme for Data Brokers in IoT Data Marketplaces",
+            authors: "Hyeontaek Oh, <em>Sangdon Park*</em>, Gyu Myoung Lee, Hwanjo Heo, and Jun Kyun Choi",
+            journal: "IEEE Access, vol. 07, pp. 40120-40132, March, 2019 (Impact Factor 3.557, 16.22% for 2017 JCR Ranking)",
+            link: "https://ieeexplore.ieee.org/document/8664564",
+            type: "journal"
+        },
+        {
+            title: "Load Profile Extraction by Mean-Shift Clustering with Sample Pearson Correlation Coefficient Distance",
+            authors: "Nakyoung Kim, <em>Sangdon Park*</em>, Joohyung Lee, and Jun Kyun Choi",
+            journal: "Energies, vol. 11, no. 09, pp. 2397, September, 2018 (Impact Factor 2.676, 49.48% for 2017 JCR Ranking)",
+            link: "https://www.mdpi.com/1996-1073/11/9/2397",
+            type: "journal"
+        },
+        {
+            title: "Energy Trading System in Microgrids with Future Forecasting and Forecasting Errors",
+            authors: "Gyohun Jeong, <em>Sangdon Park*(co-corresponding)</em>, Joohyung Lee, and Ganguk Hwang*",
+            journal: "IEEE Access, vol. 06, pp. 44094-44106, August, 2018",
+            link: "https://ieeexplore.ieee.org/document/8424153/",
+            type: "journal"
+        },
+        {
+            title: "An Optimal Pricing Scheme for the Energy Efficient Mobile Edge Computation Offloading with OFDMA",
+            authors: "Sunghwan Kim, <em>Sangdon Park*</em>, Min Chen, and Chan-hyun Youn",
+            journal: "IEEE Communications Letter, vol. 22, no. 09, pp. 1922-1925, June, 2018 (Impact Factor 2.723, 35.63% for 2017 JCR Ranking)",
+            link: "https://ieeexplore.ieee.org/document/8392375/",
+            type: "journal"
+        },
+        {
+            title: "Three Hierarchical Levels of Big-data Market Model over Multiple Data Sources for Internet of Things",
+            authors: "Busik Jang, <em>Sangdon Park*(co-corresponding)</em>, Joohyung Lee*, and Sang Geun Hahn",
+            journal: "IEEE Access, vol. 06, pp. 31269-31280, June, 2018 (Impact Factor 3.557, 16.22% for 2017 JCR Ranking)",
+            link: "https://ieeexplore.ieee.org/document/8374410/",
+            type: "journal"
+        },
+        {
+            title: "Competitive Partial Computation Offloading for Maximizing Energy Efficiency in Mobile Cloud Computing",
+            authors: "Sanghong Ahn, Joohyung Lee*, <em>Sangdon Park*(co-corresponding)</em>, S.H. Shah Newaz and Jun Kyun Choi",
+            journal: "IEEE Access, vol. 06, pp. 899-912, November, 2017 (Impact Factor 3.557, 16.22% for 2017 JCR Ranking)",
+            link: "http://ieeexplore.ieee.org/document/8119910/",
+            type: "journal"
+        },
+        {
+            title: "Event-Driven Energy Trading System in Microgrids: Aperiodic Market Model Analysis with a Game Theoretic Approach",
+            authors: "<em>Sangdon Park</em>, Joohyung Lee*, Ganguk Hwang, and Jun Kyun Choi",
+            journal: "IEEE Access, vol. 05, pp. 26291-26302, November, 2017 (Impact Factor 3.557, 16.22% for 2017 JCR Ranking)",
+            link: "http://ieeexplore.ieee.org/document/8093758/",
+            type: "journal"
+        },
+        {
+            title: "Learning-Based Adaptive Imputation Method With kNN Algorithm for Missing Power Data",
+            authors: "Minkyung Kim, <em>Sangdon Park</em>, Joohyung Lee*, Yongjae Joo, and Jun Kyun Choi",
+            journal: "Energies, vol. 10, no. 10, pp. 1668, October, 2017 (Impact Factor 2.676, 49.48% for 2017 JCR Ranking)",
+            link: "http://www.mdpi.com/1996-1073/10/10/1668",
+            type: "journal"
+        },
+        {
+            title: "Contribution Based Energy Trading Mechanism in Micro-Grids for Future Smart Grid: A Game Theoretic Approach",
+            authors: "<em>Sangdon Park</em>, Joohyung Lee*, Sohee Bae, Ganguk Hwang, and Jun Kyun Choi",
+            journal: "IEEE Transactions on Industrial Electronics, vol. 63, no. 07, pp. 4255-4265, July 2016 (Impact Factor 7.168, 1.67% for 2016 JCR Ranking)",
+            link: "http://ieeexplore.ieee.org/abstract/document/7422059/",
             type: "journal"
         },
         
@@ -285,6 +397,34 @@ function loadPublications() {
             link: "https://ieeexplore.ieee.org/document/8378441/",
             type: "conference"
         },
+        {
+            title: "Optimal Throughput Analysis of Random Access Policies for Cognitive Radio Networks with Multiple Channel Access",
+            authors: "<em>Sangdon Park</em>, Ganguk Hwang* and Jun Kyun Choi",
+            journal: "11th International Conference on Queueing Theory and Network Applications (QTNA), Wellington, New Zealand, December 23-15, 2016 (Best Student Paper Award)",
+            link: "http://dl.acm.org/citation.cfm?id=3016035",
+            type: "conference"
+        },
+        {
+            title: "Joint optimal access and sensing policy on distributed cognitive radio networks with channel aggregation",
+            authors: "<em>Sangdon Park</em>, Jaedeok Kim, Ganguk Hwang* and Jun Kyun Choi",
+            journal: "IEEE 9th International Conference on Ubiquitous and Future Networks (ICUFN), Vienna, Austria, July 5-8, 2016",
+            link: "http://ieeexplore.ieee.org/abstract/document/7537027/",
+            type: "conference"
+        },
+        {
+            title: "Maximizing energy efficiency in off-peak hours: A novel sleep scheme for WLAN access points",
+            authors: "Hyeontaek Oh, S. H. S. Newaz, <em>Sangdon Park</em> and Jun Kyun Choi*",
+            journal: "IEEE/IFIP Network Operations and Management Symposium (NOMS), Turkey, Istanbul, April 25-29 2016",
+            link: "http://ieeexplore.ieee.org/abstract/document/7502830/",
+            type: "conference"
+        },
+        {
+            title: "Energy efficient relay selection scheme with DRX mechanism in 3GPP LTE network",
+            authors: "Seonghwa Yun, Kyeongmin Lee, <em>Sangdon Park</em> and Jun Kyun Choi*",
+            journal: "IEEE International Conference on ICT Convergence (ICTC), Jeju Island, South Korea, October 14-16, 2013",
+            link: "http://ieeexplore.ieee.org/abstract/document/6675296/",
+            type: "conference"
+        },
         
         // Standards
         {
@@ -298,6 +438,20 @@ function loadPublications() {
             title: "Revised texts for a main chapter of draft recommendation Y.energyECN",
             authors: "<em>Sangdon Park</em>, Jaewon Ahn, Jun Kyun Choi, Gyu Myoung Lee",
             journal: "ITU-T SG13 Rapporteurs meeting (Geneva, 17 – 28 February 2014), SG13RGM-C-75, February 2014",
+            link: "#",
+            type: "standard"
+        },
+        {
+            title: "A proposal for definitions of energy efficiency class of network equipment (Y.energyECN)",
+            authors: "<em>Sangdon Park</em>, Gyu Myoung Lee, Jun Kyun Choi",
+            journal: "ITU-T SG13 meeting (Kampala, 4 – 15 November 2013), COM13-C401-E, November 2013",
+            link: "#",
+            type: "standard"
+        },
+        {
+            title: "Consideration of classification of sleep mode control on network equipment",
+            authors: "<em>Sangdon Park</em>, Seung Hyun Jeon, Jun Kyun Choi and Jeong Yun Kim",
+            journal: "ITU-T SG13 Rapporteurs meeting (Geneva, 17-27 June 2013), SG13RGM-C-06, June 2013",
             link: "#",
             type: "standard"
         },
